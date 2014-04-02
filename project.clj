fead5823ab2bc65704499223957a9a8efed43589
Magnar Sveen <magnars@gmail.com>
2014-04-02 13:03:44 +0200

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject optimus-img-transform "0.1.1"
+(defproject optimus-img-transform "0.2.0"
   :description "An Optimus image transformation middleware."
   :url "http://github.com/magnars/optimus-img-transform"
   :license {:name "Eclipse Public License"
